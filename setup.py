3135933baa0b21322fa4f7acada84cc8d04b64eb
Kostas Giotis <coyiotis@gmail.com>
2015-09-27 19:29:14 +0300

    dlimen files installation through setuptools

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
-from setuptools.command.install import install
-
+import distutils.cmd 
+import distutils.log
 import os
 import shutil, errno
 
@@ -12,27 +12,35 @@ def copyall(src, dst):
             shutil.copy(src, dst)
         else: raise
 
-class CustomInstallCommand(install):
-    """Customized setuptools install command - prints a friendly greeting."""
+class CustomInstallCommand(distutils.cmd.Command):
+    """Custom dLimen Installer"""
+
+    description = "dLimen installer for the upstart job"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
     def run(self):
-        print "----------dLimen Installation starting----------"
+        self.announce("----------dLimen Installation starting----------",
+        level=distutils.log.INFO)
         current_dir = os.path.dirname(os.path.realpath(__file__))
-	print "Copying dir into /usr/local/bin/dlimen"
-	copyall(current_dir, '/usr/local/bin/dlimen') 
-	print "Copying upstart job into /etc/init/dlimen.conf"
-	copyall(current_dir+'/init/dlimen.conf', '/etc/init/.')
-	install.run(self)
-
-	print "----------dLimen Installation completed---------"
-
+        print "Copying dir into /usr/local/bin/dlimen"
+        copyall(current_dir, '/usr/local/bin/dlimen')
+        print "Copying upstart job into /etc/init/dlimen.conf"
+        copyall(current_dir+'/init/dlimen.conf', '/etc/init/.')
+        print "----------dLimen Installation completed---------"
 
 setup(
+    cmdclass={
+        'dlimen': CustomInstallCommand,
+    },
     name = 'dLimen',
     version = '0.1',
-    author = "kostas",
+    author = "Kostas Giotis",
     packages = find_packages(),
-    install_requires = ["pyudev", "configparser", "hurry.filesize"],
-    cmdclass={
-        'install': CustomInstallCommand,
-    }
+    install_requires = ["pyudev", "configparser", "hurry.filesize"]
 )
